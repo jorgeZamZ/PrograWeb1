f01f9784c56726214d6354f4a2bff1cf9ec7f52d
jorgeZamZ <yoshi.zambrano@outlook.es>
2025-05-15 16:29:39 -0600

    f

--- ProyectoWeb/script.js
+++ ProyectoWeb/script.js
@@ -7,12 +7,17 @@ window.addEventListener('scroll', function() {
         navbar.style.background = 'rgb(235, 229, 204)'; // Opaco
         navbar.style.borderBottom = '2px solid #a9b57c'; // Añadir borde inferior
         navbar.style.transition = 'background 0.3s';
-        fondoprron.style.boxShadow = ''; // Quitar sombra si existe
+       
 
     } else {
         navbar.style.background = 'transparent'; // Transparente
         navbar.style.transition = 'background 0.3s';
         navbar.style.borderBottom = '0px solid rgba(169, 181, 124, 0)';
-        fondoprron.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)'; // Añadir sombra
+       
+    }
+    if (window.scrollY > 1) {
+        fondoprron.style.boxShadow = ''; // Quitar sombra si existe
+    } else {
+         fondoprron.style.boxShadow = '0 4px 8px rgba(0, 0, 0, 0.1)'; // Añadir sombra
     }
 });
